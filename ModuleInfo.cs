160ac70c42d2e7c28f2c97c85b660cf3d60f6b76
BlackOfWorld <admin@blackofworld.fun>
2021-08-26 03:30:11 +0200

    Fix horrible shit aka ModuleInfo

--- ModuleInfo.cs
+++ ModuleInfo.cs
@@ -4,48 +4,20 @@ using GLaDOSV3.Helpers;
 using System;
 using System.Reflection;
 using System.Runtime.Loader;
-using GLaDOSV3.Models.Interfaces;
+using GLaDOSV3.Models;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace GLaDOSV3.Module.Music
 {
-    public class ModuleInfo : IGladosModule
+    public class ModuleInfo : GladosModule
     {
-        public string Name() => "Music";
+        public override string Name => "Music";
+               
+        public override string Version => "0.0.0.1";
+               
+        public override string Author => "BlackOfWorld#8125";
 
-        public string Version() => "0.0.0.1";
+        public override Type[] Services(DiscordShardedClient discord, CommandService commands, BotSettingsHelper<string> config, IServiceCollection provider) => new Type[] { typeof(AudioService)};
 
-        public Uri UpdateUrl() => null;
-
-        public string Author() => "BlackOfWorld#8125";
-
-        public Type[] Services(DiscordShardedClient discord, CommandService commands, BotSettingsHelper<string> config, IServiceCollection provider) => new Type[] { typeof(AudioService)};
-
-        private static volatile ModuleInfo _singleton;
-        public static IGladosModule GetModule()
-        {
-            if (_singleton != null) return _singleton;
-            _singleton = new ModuleInfo();
-            Assembly currentAssembly = Assembly.GetExecutingAssembly();
-            AssemblyLoadContext currentContext = AssemblyLoadContext.GetLoadContext(currentAssembly);
-            currentContext.Unloading += OnPluginUnloadingRequested;
-            return _singleton;
-        }
-
-        public void PreLoad(DiscordShardedClient discord, CommandService commands, BotSettingsHelper<string> config, IServiceProvider provider)
-        {
-        }
-        public void PostLoad(DiscordShardedClient discord, CommandService commands, BotSettingsHelper<string> config, IServiceProvider provider)
-        {
-        }
-
-        public void Reload(DiscordShardedClient discord, CommandService commands, BotSettingsHelper<string> config, IServiceProvider provider)
-        { }
-
-        public void Unload(DiscordShardedClient discord, CommandService commands, BotSettingsHelper<string> config, IServiceProvider provider)
-        { }
-
-        public static void OnPluginUnloadingRequested(AssemblyLoadContext obj)
-        { }
     }
 }
